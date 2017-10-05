08cc76c35c03d8a03ba0bdd8cc06bfe3ad40fe27
kittipanP <kittipan.prasertsang@gmail.com>
2017-10-05 18:20:01 +0700

    designed main page

--- index.php
+++ index.php
@@ -1,3 +1,127 @@
-<?php 
-echo "hello judging";
-?>
+<!DOCTYPE html>
+<html>
+<title>Judging</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+
+
+<link rel="icon" type="image/png" href="img/logo/wd.png"/>
+<body>
+
+
+
+<div class="w3-container w3-center">
+  <hr>
+  <h3>Paper-like Cards with Shadows</h3>
+</div>
+
+<div class="w3-row-padding"> 
+
+<div class="w3-third">
+<div class="w3-card-2">
+                            <div class="w3-display-container ">
+                                <div class="" style="min-height:">
+  								  <a href="#" class = "textdecoration">
+                                    <img src="img/images/car.jpg" class="w3-hover-opacity" style="width:100%">
+                                      <div class="w3-display-middle w3-display-hover"> 
+                                        <div class="w3-panel w3-orange w3-card-2 ">
+                                          <a href="#" class = "textdecoration"><p>Engineering</p></a>
+                                        </div>
+                                      </div>
+                                  </a> 
+                                </div>
+                            </div>
+  <div class="w3-container">
+  <p>Advance Process Technology</p>
+  </div>
+</div>
+</div>
+
+<div class="w3-third">
+<div class="w3-card-4">
+                            <div class="w3-display-container ">
+                                <div class="" style="min-height:">
+                                  <a href="" class = "textdecoration">
+                                    <img src="img/images/car-i.jpg" class="w3-hover-opacity" style="width:100%">
+
+                                  <div class="w3-display-topright w3-display-hover"> 
+                                    <div class="w3-panel w3-blue-grey w3-card-4 " >
+                                      <a href=""><p>&nbsp;&nbsp;&nbsp;&nbsp;Engineering</p></a>
+                                    </div>
+                                  </div>
+
+
+                                  </a>   
+                                </div>
+                            </div>
+                            <!-- On Process ii -->
+                            <div class="w3-display-container">
+                                <div class="" style="min-height:">
+                                  <a href="" class = "textdecoration">
+                                    <img src="img/images/car-ii.jpg" class="w3-hover-opacity" style="width:100%">
+
+
+
+                                  <div class="w3-display-topright w3-display-hover"> 
+                                    <div class="w3-panel w3-blue-grey w3-card-4 " >
+                                      <a href="#"><p>DL, Tech, Staff</p></a>
+                                    </div>
+                                  </div>
+
+
+                                  </a>   
+                                </div>
+                            </div>
+  <div class="w3-container">
+  <p>Quality</p>
+  </div>
+</div>
+</div>
+
+<div class="w3-third">
+<div class="w3-card-4">
+                            <div class="w3-display-container ">
+                                <div class="" style="min-height:">
+                                  <a href="" class = "textdecoration">
+                                    <img src="img/images/car-i.jpg" class="w3-hover-opacity" style="width:100%">
+
+                                  <div class="w3-display-topright w3-display-hover"> 
+                                    <div class="w3-panel w3-blue-grey w3-card-4 " >
+                                      <a href=""><p>&nbsp;&nbsp;&nbsp;&nbsp;Engineering</p></a>
+                                    </div>
+                                  </div>
+
+
+                                  </a>   
+                                </div>
+                            </div>
+                            <!-- On Process ii -->
+                            <div class="w3-display-container">
+                                <div class="" style="min-height:">
+                                  <a href="" class = "textdecoration">
+                                    <img src="img/images/car-ii.jpg" class="w3-hover-opacity" style="width:100%">
+
+
+
+                                  <div class="w3-display-topright w3-display-hover"> 
+                                    <div class="w3-panel w3-blue-grey w3-card-4 " >
+                                      <a href="#"><p>DL, Tech, Staff</p></a>
+                                    </div>
+                                  </div>
+
+
+                                  </a>   
+                                </div>
+                            </div>
+  <div class="w3-container">
+  <p>Operations/Yield/Cost/People</p>
+  </div>
+</div>
+</div>
+</div>
+<br>
+</body>
+</html>
