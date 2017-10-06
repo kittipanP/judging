9129b513fdd0db78685de266ed71ec80c81df9e5
kittipanP <kittipan.prasertsang@gmail.com>
2017-10-06 11:46:56 +0700

    created a page for evaluation form

---
+++ evaluation.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>jQuery | Custom Radio Buttons Survey</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css?v=23999">
+
+  
+</head>
+
+<body>
+  <div class="rb-box">
+
+  <!-- Radio Button Module -->
+  <p>1. </p>
+  <div id="rb-1" class="rb">
+    <div class="rb-tab" data-value="1">
+      <div class="rb-spot">
+        <span class="rb-txt">1</span>
+      </div>
+    </div><div class="rb-tab" data-value="2">
+      <div class="rb-spot">
+        <span class="rb-txt">2</span>
+      </div>
+    </div><div class="rb-tab" data-value="3">
+      <div class="rb-spot">
+        <span class="rb-txt">3</span>
+      </div>
+    </div><div class="rb-tab" data-value="4">
+      <div class="rb-spot">
+        <span class="rb-txt">4</span>
+      </div>
+    </div><div class="rb-tab" data-value="5">
+      <div class="rb-spot">
+        <span class="rb-txt">5</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Radio Button Module -->
+  <p>2. </p>
+  <div id="rb-2" class="rb">
+    <div class="rb-tab" data-value="1">
+      <div class="rb-spot">
+        <span class="rb-txt">1</span>
+      </div>
+    </div><div class="rb-tab " data-value="2">
+      <div class="rb-spot">
+        <span class="rb-txt">2</span>
+      </div>
+    </div><div class="rb-tab" data-value="3">
+      <div class="rb-spot">
+        <span class="rb-txt">3</span>
+      </div>
+    </div><div class="rb-tab" data-value="4">
+      <div class="rb-spot">
+        <span class="rb-txt">4</span>
+      </div>
+    </div><div class="rb-tab" data-value="5">
+      <div class="rb-spot">
+        <span class="rb-txt">5</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Radio Button Module -->
+  <p>3. </p>
+  <div id="rb-3" class="rb">
+    <div class="rb-tab" data-value="1">
+      <div class="rb-spot">
+        <span class="rb-txt">1</span>
+      </div>
+    </div><div class="rb-tab" data-value="2">
+      <div class="rb-spot">
+        <span class="rb-txt">2</span>
+      </div>
+    </div><div class="rb-tab " data-value="3">
+      <div class="rb-spot">
+        <span class="rb-txt">3</span>
+      </div>
+    </div><div class="rb-tab" data-value="4">
+      <div class="rb-spot">
+        <span class="rb-txt">4</span>
+      </div>
+    </div><div class="rb-tab" data-value="5">
+      <div class="rb-spot">
+        <span class="rb-txt">5</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Radio Button Module -->
+  <p>4. </p>
+  <div id="rb-4" class="rb">
+    <div class="rb-tab" data-value="1">
+      <div class="rb-spot">
+        <span class="rb-txt">1</span>
+      </div>
+    </div><div class="rb-tab" data-value="2">
+      <div class="rb-spot">
+        <span class="rb-txt">2</span>
+      </div>
+    </div><div class="rb-tab" data-value="3">
+      <div class="rb-spot">
+        <span class="rb-txt">3</span>
+      </div>
+    </div><div class="rb-tab " data-value="4">
+      <div class="rb-spot">
+        <span class="rb-txt">4</span>
+      </div>
+    </div><div class="rb-tab" data-value="5">
+      <div class="rb-spot">
+        <span class="rb-txt">5</span>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Button -->
+  <div class="button-box">
+    <button class="button trigger">Submit!</button>
+  </div>
+
+</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
